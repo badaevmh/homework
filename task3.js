e5f6b51f3b41d5ff5018d126703095ca242faf1e
Muhammad-Husein <38377122+badaevmh@users.noreply.github.com>
2022-12-02 21:08:49 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,10 +1,42 @@
 'use strict';
 
+const person = {
+    name: 'Lom-Ali',
+    age: '18',
+    dateOfBirth: '2004',
+    salary: 200000,
+    department: 'marketing'
+}
+
+const person2 = {
+    name: 'Saad',
+    age: '28',
+    dateOfBirth: '1999',
+    salary: 100000,
+    department: 'development'
+}
+
+const person3 = {
+    name: 'Muhammad-Huseyn',
+    age: '25',
+    dateOfBirth: '1997',
+    salary: 100,
+    department: 'development'
+}
+
+const person4 = {
+    name: 'Muhammad-Huseyn2',
+    age: '25',
+    dateOfBirth: '1997',
+    salary: 200,
+    department: 'development'
+}
+
 class Human {
-    constructor(name, age, dateOfBirth) {
-        this.name = String(name); // можно через тернарный оператор запилить проверку типа данных, но нужно ли по заданию не понял
-        this.age = Number(age);
-        this.dateOfBirth = String(dateOfBirth);
+    constructor(person) {
+        this.name = String(person.name); // можно через тернарный оператор запилить проверку типа данных, но нужно ли по заданию не понял
+        this.age = Number(person.age);
+        this.dateOfBirth = String(person.dateOfBirth);
     }
 
     displayInfo() {
@@ -13,22 +45,22 @@ class Human {
 }
 
 class Employee extends Human {
-    constructor(name, age, dateOfBirth, salary, department) {
-        super(name, age, dateOfBirth);
-        this.salary = Number(salary);
-        this.department = String(department);
+    constructor(person) {
+        super(person);
+        this.salary = Number(person.salary);
+        this.department = String(person.department);
     }
 
     displayInfo() {
-        return `${super.displayInfo()}, Зарплата:${this.salary}, Отдел:${this.department}`;
+        return `${super.displayInfo()}, Зарплата: ${this.salary}, Отдел: ${this.department}`;
     }
 
 }
 
 class Manager extends Employee {
-    constructor(name, age, dateOfBirth, salary, department, devList = []) {
-        super(devList);
-        this.devList = devList;
+    constructor(person) {
+        super(person);
+        this.devList = [];
     }
 
     addDev(dev) {
@@ -44,27 +76,26 @@ class Manager extends Employee {
 }
 
 class Developer extends Employee {
-    constructor(name, age, dateOfBirth, salary, department) {
-        super(name, age, dateOfBirth, salary, department);
+    constructor(person) {
+        super(person);
+        this.thisManager = null;
     }
 
     changeManager(manager) {
         this.manager = manager;
+        manager.addDev(this);
     }
 }
 
-const human1 = new Human('Имя', 35, '12.11.1997');
-const employee1 = new Employee('Имя', 35, '12.11.1997', '30000', 'Веб-разработка');
-const manager1 = new Manager('Саад', 25, '12.11.1997', '60000', 'Маркетинг');
-const manager2 = new Manager('Джанибек', 35, '12.11.1997', '40000', 'Маркетинг');
-const dev1 = new Developer('Лев', 26, '12.11.1997', '130000', 'Front-end');
-const dev2 = new Developer('Лом-Али', 17, '12.11.1997', '160000', 'Back-end');
 
-manager1.addDev(dev1.name);
-manager1.addDev(dev2.name);
-manager2.addDev(dev1.name);
-manager2.addDev(dev2.name);
-manager1.deleteDev(dev1.name);
+const manager1 = new Manager(person);
+const manager2 = new Manager(person2);
+const dev1 = new Developer(person3);
+const dev2 = new Developer(person4);
+
+manager1.addDev(person3);
+console.log(manager1.devList);
+manager1.deleteDev(person3);
 console.log(manager1.devList);
 
 console.log(employee1.displayInfo())
